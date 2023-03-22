b61245f32530c4f309b1731f5f635c3f80777851
Sridhar Ratnakumar <srid@srid.ca>
2023-03-22 15:05:56 -0400

    Fix

--- flake.nix
+++ flake.nix
@@ -65,15 +65,16 @@
               set -euo pipefail
 
               set -x
-              docker load -i $(nix build "$1" --print-out-paths)
+              docker load -i "$(nix build "$1" --print-out-paths)"
               set +x
-              IMAGE_NAME=`$(nix eval --raw .#packages.x86_64-linux.$1.buildArgs.name):$(nix eval --raw .#packages.x86_64-linux.$1.buildArgs.tag)`
+              IMAGE_NAME="$(nix eval --raw .#packages.x86_64-linux."$1".buildArgs.name):$(nix eval --raw .#packages.x86_64-linux."$1".buildArgs.tag)"
               echo "Loaded image: ''${IMAGE_NAME}"
 
               echo "Logging in to Docker Registry"
-              export HOME=$(mktemp -d)
+              HOME="$(mktemp -d)"
+              export HOME
               trap 'rm -rf "$HOME"'  EXIT
-              echo ''${DOCKER_PASS} | docker login -u ''${DOCKER_USER} --password-stdin ''${DOCKER_SERVER}
+              echo "''${DOCKER_PASS}" | docker login -u "''${DOCKER_USER}" --password-stdin "''${DOCKER_SERVER}"
               set -x
               docker push "''${IMAGE_NAME}"
               set +x
