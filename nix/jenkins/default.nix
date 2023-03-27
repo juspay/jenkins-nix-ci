250b0f1fdcae557eb80dbcd1d4dee1870aad3d49
Sridhar Ratnakumar <srid@srid.ca>
2023-03-27 17:49:26 -0400

    Split jenkins/default.nix

--- nix/jenkins/default.nix
+++ nix/jenkins/default.nix
@@ -5,6 +5,8 @@
   imports = [
     ./casc.nix
     ./features
+    ./local-node.nix
+    (import ./plugins.nix { inherit jenkinsPlugins2nix; })
   ];
   options.jenkins-nix-ci = lib.mkOption {
     type = lib.types.submodule {
@@ -17,42 +19,6 @@
         domain = lib.mkOption {
           type = lib.types.str;
           description = "The domain in which Jenkins is exposed to the outside world.";
-        };
-        plugins = lib.mkOption {
-          type = lib.types.listOf lib.types.str;
-          default = [ ];
-          description = "A list of plugins to install.";
-        };
-        plugins-file = lib.mkOption {
-          type = lib.types.str;
-          default = null;
-          description = ''
-            Path to the generated Nix expression containing the plugins.
-
-            Must be relative to project root.
-          '';
-        };
-        nix-prefetch-jenkins-plugins = lib.mkOption {
-          type = lib.types.functionTo lib.types.package;
-          default = pkgs:
-            let
-              jenkinsPlugins2nix_system =
-                if pkgs.system == "aarch64-darwin" then "x86_64-darwin" else pkgs.system;
-              fetcher = jenkinsPlugins2nix.packages.${jenkinsPlugins2nix_system}.jenkinsPlugins2nix;
-              inherit (config.jenkins-nix-ci) plugins;
-            in
-            pkgs.writeShellApplication {
-              name = "nix-prefetch-jenkins-plugins";
-              text = ''
-                ${lib.getExe fetcher} \
-                  ${lib.foldl (a: b: "${a} -p ${b}") "" plugins}
-              '';
-            };
-          description = ''
-            The program that creates `plugins.nix` based on given plugins.
-
-            This will fetch the latest plugins using jenkinsPlugins2Nix.
-          '';
         };
       };
     };
@@ -84,16 +50,5 @@
         "-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true"
       ];
     };
-
-    # TODO: The following configuration will be moved to slaves when we set them up.
-
-    # To allow the local node to run as builder, supporting nix builds.
-    # This should not be necessary with external build agents.
-    nix.settings.allowed-users = [ "jenkins" ];
-    nix.settings.trusted-users = [ "jenkins" ];
-
-    # Install docker so we can build images.
-    virtualisation.docker.enable = lib.mkIf config.jenkins-nix-ci.features.docker.enable true;
-    services.jenkins.extraGroups = lib.optionals config.jenkins-nix-ci.features.docker.enable [ "docker" ];
   };
 }
