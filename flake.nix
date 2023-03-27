f2de1b05a80bbcca317ae32bc8eff5c5dc1538fd
Sridhar Ratnakumar <srid@srid.ca>
2023-03-27 13:53:30 -0400

    Put nammayatri configuration in its own dir

--- flake.nix
+++ flake.nix
@@ -1,73 +1,18 @@
 {
   inputs = {
+    jenkinsPlugins2nix.url = "github:Fuuzetsu/jenkinsPlugins2nix";
+
+    # Inputs for ./example/nammayatri
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    deploy-rs.url = "github:serokell/deploy-rs";
-    jenkinsPlugins2nix.url = "github:Fuuzetsu/jenkinsPlugins2nix";
     nixos-flake.url = "github:srid/nixos-flake";
     sops-nix.url = "github:Mic92/sops-nix";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
-  outputs = inputs@{ self, flake-parts, ... }:
+  outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       flake.nixosModules.default = import ./nix/jenkins.nix { inherit (inputs) jenkinsPlugins2nix; };
-
-      # TODO: Everything below (including some imports above) should be moved to
-      # ./example
-
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      imports = [
-        inputs.nixos-flake.flakeModule
-        ./nix/ngrok-outputs.nix
-        ./nix/deploy.nix
-      ];
-      # System configuration
-      flake.nixosConfigurations.jenkins-nix-ci = self.nixos-flake.lib.mkLinuxSystem ({ pkgs, config, ... }: {
-        imports = [
-          inputs.sops-nix.nixosModules.sops
-
-          # Jenkins module usage
-          self.nixosModules.default
-          ({
-            jenkins-nix-ci = {
-              # Hardcoded domain spit out by ngrok
-              domain = "b149-106-51-91-112.in.ngrok.io";
-              plugins = [
-                "github-api"
-                "git"
-                "github-branch-source"
-                "workflow-aggregator"
-                "ssh-slaves"
-                "configuration-as-code"
-              ];
-              # This file can be updated by running:
-              #   nix-prefetch-jenkins-plugins > nix/jenkins/plugins.nix
-              plugins-file = "nix/jenkins/plugins.nix";
-            };
-          })
-
-          ./nix/configuration.nix
-          ./nix/ngrok.nix
-        ];
-        sops.defaultSopsFile = ./secrets.yaml;
-      });
-
-      perSystem = { self', inputs', system, lib, config, pkgs, ... }: {
-        formatter = pkgs.nixpkgs-fmt;
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nixpkgs-fmt
-            inputs'.deploy-rs.packages.default
-            pkgs.sops
-            (self.nixosConfigurations.jenkins-nix-ci.config.jenkins-nix-ci.nix-prefetch-jenkins-plugins pkgs)
-          ];
-        };
-
-        # Library of apps to run in `Jenkinsfile`
-        # TODO: Remove after this PR is merged: https://github.com/nammayatri/nammayatri/pull/284
-        packages = {
-          docker-push = pkgs.callPackage ./groovy-library/vars/dockerPush.nix { };
-          cachix-push = pkgs.callPackage ./groovy-library/vars/cachixPush.nix { };
-        };
-      };
+      # TODO: Move example to independent flake
+      imports = [ ./example/nammayatri ];
     };
 }
