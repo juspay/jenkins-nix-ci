2099a1a563fb334003b0219291499b9a037c87ca
Sridhar Ratnakumar <srid@srid.ca>
2023-03-28 10:37:30 -0400

    Remove flake-parts dependency

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   inputs = {
     jenkinsPlugins2nix.url = "github:Fuuzetsu/jenkinsPlugins2nix";
   };
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      flake.nixosModules.default = import ./nix/jenkins { inherit (inputs) jenkinsPlugins2nix; };
-    };
+  outputs = inputs: {
+    nixosModules.default = import ./nix/jenkins { inherit (inputs) jenkinsPlugins2nix; };
+  };
 }
