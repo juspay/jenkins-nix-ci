71003fbaaba8a17e02bc74c70504ebacc6a5818c
Sridhar Ratnakumar <srid@srid.ca>
2023-04-24 13:58:15 -0400

    cachixPush: push all outputs (via devour-flake)
    Resolves #14 (using different approach)

--- nix/jenkins/features/cachix/cachixPush.nix
+++ nix/jenkins/features/cachix/cachixPush.nix
@@ -12,18 +12,10 @@ pkgs.writeShellApplication {
 
     CACHE="$1"
 
-    # Push the .#default package.
-    # TODO: We want to push *all* packages.
-    # cf. https://github.com/NixOS/nix/issues/7165
     set -x
-    nix build --json \
-      | jq -r '.[].outputs | to_entries[].value' \
-      | cachix push "''${CACHE}"
-
-    # Push devshell.
-    # Clear all but $PATH, to prevent secrets leaking to shellHook.
-    env -i PATH="$PATH" nix develop --no-update-lock-file --profile dev-profile -c echo
-    cachix push "''${CACHE}" dev-profile
-    rm -f dev-profile*
+    nix build github:srid/devour-flake \
+      -L --no-link --print-out-paths \
+      --override-input flake . \
+      | xargs cat | cachix push "''${CACHE}" 
   '';
 }
