ce12c3c8593b49f0b82f6f3a16cffbaec08229a0
Sridhar Ratnakumar <srid@srid.ca>
2023-05-03 15:50:19 -0400

    nixBuildAll: Add 'system' argument

--- nix/jenkins/features/nix/nixBuildAll.groovy
+++ nix/jenkins/features/nix/nixBuildAll.groovy
@@ -1,14 +1,17 @@
 #!/usr/bin/env groovy
 
-def call() {
-    stage ("Nix Build All") {
-      sh '''
+def call(Map args = [:]) {
+    system = args["system"] ?: null
+    systemStr = (system == null) ? "default" : system
+    stage ("Nix Build All (${systemStr})") {
+      nixArgs = (system == null) ? "" : "--option system ${system}"
+      sh """
         # Make sure that flake.lock is sync
         nix flake lock --no-update-lock-file
 
         # Do a full nix build (all outputs)
         # This uses https://github.com/srid/devour-flake
-        devour-flake .
-        '''
+        devour-flake . ${nixArgs}
+        """
     }
 }
