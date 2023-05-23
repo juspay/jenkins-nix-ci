8053f97eeefcc17fd9f2c846ad0a8859a1461d28
Sridhar Ratnakumar <srid@srid.ca>
2023-05-23 15:50:48 -0400

    Update nixpkgs and use cachix 1.5

--- flake.nix
+++ flake.nix
@@ -2,14 +2,11 @@
   inputs = {
     jenkinsPlugins2nix.url = "github:Fuuzetsu/jenkinsPlugins2nix";
     flake-outputs.url = "github:nix-systems/flake-outputs";
-    # Downgrade cachix to obviate https://github.com/cachix/cachix/issues/529
-    cachix.url = "github:cachix/cachix/v1.3.3";
     devour-flake.url = "github:srid/devour-flake";
     devour-flake.flake = false;
   };
   outputs = inputs: {
     overlay = self: super: {
-      cachix = inputs.cachix.packages.${self.system}.default;
       flake-outputs = inputs.flake-outputs.packages.${self.system}.default;
       devour-flake = self.callPackage inputs.devour-flake { };
       jenkinsPlugins2nix = inputs.jenkinsPlugins2nix.packages.${if self.system == "aarch64-darwin" then "x86_64-darwin" else self.system}.jenkinsPlugins2nix;
