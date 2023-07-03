24db005848d27ff1b273ff869c9b65d63f954a46
Sridhar Ratnakumar <srid@srid.ca>
2023-07-03 16:41:49 -0400

    nixBuildAll: ability to build subflakes
    Resolves #27

--- nix/jenkins/features/nix/nixBuildAll.groovy
+++ nix/jenkins/features/nix/nixBuildAll.groovy
@@ -1,11 +1,15 @@
 #!/usr/bin/env groovy
 
 def call(Map args = [:]) {
-    system = args["system"] ?: null
-    systemStr = (system == null) ? "default" : system
-    stage ("Nix Build All (${systemStr})") {
-      nixArgs = (system == null) ? "" : "--option system ${system}"
-      flakeOutputs = sh script: "nix-build-all ${nixArgs}",
+    system = args["system"]
+    flakeDir = args["flakeDir"] ?: "."
+    overrideInputs = args["overrideInputs"] ?: [:]
+    stage ("""Nix Build All (${system ?: "default"})""") {
+      nixArgs = [
+        system ? "--option system ${system}" : "",
+        (overrideInputs.collect { k, v -> "--override-input flake/${k} ${v}"}.join(" "))
+      ]
+      flakeOutputs = sh script: """nix-build-all ${flakeDir} ${nixArgs.join(" ")}""",
          returnStdout: true
       echo flakeOutputs
       env.FLAKE_OUTPUTS = flakeOutputs.trim()
--- nix/jenkins/features/nix/nixBuildAll.nix
+++ nix/jenkins/features/nix/nixBuildAll.nix
@@ -7,11 +7,13 @@ pkgs.writeShellApplication {
     pkgs.devour-flake
   ];
   text = ''
+    set -x
+
     # Make sure that flake.lock is sync
     nix flake lock --no-update-lock-file
 
     # Do a full nix build (all outputs)
     # This uses https://github.com/srid/devour-flake
-    devour-flake . "$@"
+    devour-flake "$@"
   '';
 }
